a1e27f690e42c404070fa43e51c54826e725fe59
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-07-15 06:45:03 +0100

    chore: added fadeIn text effect with acerternity ui

---
+++ components/ui/GenerateTextEffect.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const TextGenerateEffect = ({
+  words,
+  text,
+  time,
+  className,
+}: {
+  words: string;
+  words2?: string;
+  word3?: string;
+  className?: string;
+  text: string;
+  time: number;
+}) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ");
+  useEffect(() => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+      },
+      {
+        duration: 2,
+        delay: stagger(time),
+      }
+    );
+  }, [scope.current]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => {
+          return (
+            <motion.span
+              key={word + idx}
+              className="dark:text-white text-black opacity-0"
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    );
+  };
+
+  return (
+    <div className={cn("font-bold", className)}>
+      <div className="mt-4">
+        <div
+          className={`dark:text-white font-mullish text-main_black text-${text} leading-snug tracking-wide`}
+        >
+          {renderWords()}
+        </div>
+      </div>
+    </div>
+  );
+};
